375f70ea5c85eb2be613b7b9fbbfeae93668dfa7
Avery <cbra067@lasierra.edu>
2018-11-12 17:12:03 -0800

    Second commit, still needs styling, error handling, firebase.  Just took out some unused lines of code and added comments.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
 
+// Variables used for user input
 let trainNameInputDiv = $("#add-train-name");
 let destinationInputDiv = $("#add-destination");;
 let firstTrainInputDiv = $("#add-first-train");
 let frequencyInputDiv = $("#add-frequency");
 let submitButton = $("#submit");
 
+// Values extracted from user input
 let trainNameVal = "";
 let destinationVal = "";
 let firstTrainVal = "";
@@ -15,56 +17,28 @@ let nextArrivalMin = "";
 
 let trainTableRow = "";
 
+//moment.js time variables
 let timeFormat = "hh:mm";
 let convertedTime = "";
 let minPast = "";
 let minAway = "";
 
-// function train (name, destination, firstTrain, frequency) {
-//     this.name = name;
-//     this.destination = destination;
-//     this.firstTrain = firstTrain;
-//     this.frequency = frequency;
-
-//     // this code inside click function
-//     // let ourNewTrain = new train(trainNameVal, destinationVal, firstTrainVal, frequencyVal);
-//     // console.log(ourNewTrain);
-// }
-
-
-// let ourTime = "06:00";
-// let ourTimeFormat = "hh:mm";
-// let convertedTime = moment(ourTime, ourTimeFormat);
-
-// let minPast = (-1 * convertedTime.diff(moment(), "minutes"));
-// console.log(minPast);
-// let minAway = frequency - (minPast % frequency);
-
-// console.log(minAway)
-
+// function to create data in table row on button click
 function createTableData(text) {
     let tableData = $("<td>");
     tableData.text(text);
     tableData.appendTo(trainTableRow);
 }
+// All time calculations used
 function timeCalc() {
     convertedTime = moment(firstTrainVal, timeFormat);
     minPast = (-1 * convertedTime.diff(moment(), "minutes"));
     console.log(minPast);
     minAway = frequencyVal - (minPast % frequencyVal);
     console.log(minAway);
-
-    // Your moment;
-
-    // Your moment at midnight
     var mmtMidnight = convertedTime.clone().startOf('day');
-
-    // Difference in minutes
     var diffMinutes = convertedTime.diff(mmtMidnight, 'minutes');
-
     console.log(diffMinutes);
-
-
     nextArrivalMin = diffMinutes + minPast + minAway;
     nextArrivalHours = Math.floor(nextArrivalMin/60);
     nextArrivalModMin = nextArrivalMin % 60;
@@ -72,19 +46,15 @@ function timeCalc() {
         nextArrivalModMin = "0" + nextArrivalModMin;
     }
     nextArrival = nextArrivalHours + ":" + nextArrivalModMin;
-    // nextArrivalMinString = nextArrivalMin.toString();
-    // nextArrival = moment(nextArrivalMin, "minutes");
-    // nextArrival.format("hh:mm:ss");
-
 }
+// used to get values
 function getValues() {
     trainNameVal = trainNameInputDiv.val();
     destinationVal = destinationInputDiv.val();
     firstTrainVal = firstTrainInputDiv.val();
     frequencyVal = frequencyInputDiv.val();
-    timeCalc();
 }
-
+// creates a row each time button is clicked
 function createTableRow() {
     trainTableRow = $("<tr>");
     createTableData(trainNameVal);
@@ -94,9 +64,11 @@ function createTableRow() {
     createTableData(minAway);
     trainTableRow.appendTo("#train-table");
 }
+// submit button function
 submitButton.on("click",function(event) {
     event.preventDefault();
     getValues();
+    timeCalc();
     createTableRow();
 
 });
